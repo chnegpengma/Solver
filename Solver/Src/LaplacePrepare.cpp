e1b0392edf4bff472d4e4e35105caf2096c1931e
unknown <3209894113@qq.com>
2024-12-23 21:54:15 +0800

    add files

--- Solver/Src/LaplacePrepare.cpp
+++ Solver/Src/LaplacePrepare.cpp
@@ -36,18 +36,28 @@ void LaplacePrepare::DoProcess()
     {
         std::vector<int> tri = matTri[i];
         Triangle oTri(vecNode[tri[0]], vecNode[tri[1]], vecNode[tri[2]]);
-        double a1 = oTri.VecCoor(2, 1) * oTri.VecCoor(3, 2) - oTri.VecCoor(3, 1) * oTri.VecCoor(2, 2);
-        double b1 = oTri.VecCoor(2, 2) - oTri.VecCoor(3, 2);
-        double c1 = oTri.VecCoor(3, 1) - oTri.VecCoor(2, 1);
-        double a2 = oTri.VecCoor(3, 1) * oTri.VecCoor(1, 2) - oTri.VecCoor(1, 1) * oTri.VecCoor(3, 2);
-        double b2 = oTri.VecCoor(3, 2) - oTri.VecCoor(1, 2);
-        double c2 = oTri.VecCoor(1, 1) - oTri.VecCoor(3, 1);
-        double a3 = oTri.VecCoor(1, 1) * oTri.VecCoor(2, 2) - oTri.VecCoor(2, 1) * oTri.VecCoor(1, 2);
-        double b3 = oTri.VecCoor(1, 2) - oTri.VecCoor(2, 2);
-        double c3 = oTri.VecCoor(2, 1) - oTri.VecCoor(1, 1);
-        std::vector<double> aiVec;
-        std::vector<double> ajVec;
-        std::vector<double> amVec;
+        Matrix<double> matBase = oTri.GetBaseMat();
+        double a1 = matBase(1, 1);
+        double b1 = matBase(1, 2);
+        double c1 = matBase(1, 3);
+        double a2 = matBase(2, 1);
+        double b2 = matBase(2, 2);
+        double c2 = matBase(2, 3);
+        double a3 = matBase(3, 1);
+        double b3 = matBase(3, 2);
+        double c3 = matBase(3, 3);
+
+        Matrix<double> matK(3, 3);
+        matK(1, 1) = (b1 * b1 + c1 * c1) / (4 * oTri.GetArea());
+        matK(1, 2) = (b1 * b2 + c1 * c2) / (4 * oTri.GetArea());
+        matK(1, 3) = (b1 * b3 + c1 * c3) / (4 * oTri.GetArea());
+        matK(2, 1) = matK(1, 2);
+        matK(2, 2) = (b2 * b2 + c2 * c2) / (4 * oTri.GetArea());
+        matK(2, 3) = (b2 * b3 + c2 * c3) / (4 * oTri.GetArea());
+        matK(3, 1) = matK(1, 3);
+        matK(3, 2) = matK(2, 3);
+        matK(3, 3) = (b3 * b3 + c3 * c3) / (4 * oTri.GetArea());
+
 
     }
 }
